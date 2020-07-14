4a0a213d27bef839e08b1a0d250b8ba9d074af3f
apzivile <zivile.apo@gmail.com>
2020-07-14 17:31:06 +0100

    Tic tac toe with ai; finished 1st part

--- Tic-Tac-Toe with AI/task/src/tictactoe/Main.java
+++ Tic-Tac-Toe with AI/task/src/tictactoe/Main.java
@@ -5,12 +5,27 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
+    private static int xAmount = 0;
+    private static int oAmount = 0;
+    private static int emptyAmount = 0;
+
+    // int xAmount=0;
+    // int oAmount = 0;
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in).useDelimiter("");
         String[][] matrix = new String[3][3];
         for (int i = 0; i < matrix.length; ++i) {
             for (int j = 0; j < matrix.length; ++j) {
                 matrix[i][j] = scanner.next();
+                if (matrix[i][j].equals("_")) {
+                    emptyAmount++;
+                }
+                if (matrix[i][j].equals("X")) {
+                    xAmount++;
+                }
+                if (matrix[i][j].equals("O")) {
+                    oAmount++;
+                }
             }
         }
 
@@ -26,7 +41,16 @@ public class Main {
         for (String[] strings : matrix) {
             System.out.print("|" + " ");
             for (int j = 0; j < matrix.length; ++j) {
+                // if (strings[j].equals("_")){
+                //   emptyAmount++;
+                //}
                 System.out.print(strings[j].replace("_", " ") + " ");
+                //  if (strings[j].equals("X")){
+                //      xAmount++;
+                //  }
+                //  if (strings[j].equals("O")){
+                //      oAmount++;
+                //  }
             }
             System.out.print("|");
             System.out.println();
@@ -42,8 +66,11 @@ public class Main {
         boolean oWon = false;
         boolean empty = false;
         boolean draw = false;
+        boolean loop = false;
+        boolean notFinished = false;
         int count = 0;
         do {
+            loop=false;
             try {
                 count++;
                 Scanner sc = new Scanner(System.in);
@@ -53,16 +80,25 @@ public class Main {
                 int xCoordinate = sc.nextInt();
                 int yCoordinate = sc.nextInt();
 
+                if (xAmount> oAmount){
+                    xTurn=false;
+                }
+                // else {
+                //       xTurn=true;
+                //  }
+
                 if (xCoordinate >= 1 && xCoordinate <= 3 && yCoordinate >= 1 && yCoordinate <= 3) {
 
                     if (matrix[Math.abs(3 - yCoordinate)][Math.abs(xCoordinate - 1)].equals("_")) {
-
+                        emptyAmount--;
                         if (xTurn) {
                             matrix[Math.abs(3 - yCoordinate)][Math.abs(xCoordinate - 1)] = "X";
+                            xAmount++;
                             isChanged = true;
                             xTurn = false;
                         } else {
                             matrix[Math.abs(3 - yCoordinate)][Math.abs(xCoordinate - 1)] = "O";
+                            oAmount++;
                             isChanged = true;
                             xTurn = true;
 
@@ -70,6 +106,7 @@ public class Main {
                         // break;
                     } else {
                         System.out.println("This cell is occupied! Choose another one!");
+                        loop=true;
                         isChanged = false;
                     }
                     xWon = victory(matrix, "X");
@@ -77,8 +114,16 @@ public class Main {
                     if (xWon || oWon) {
                         won = true;
                     }
-                    if (count==9){
-                        draw =true;
+                    if (emptyAmount==0) {
+                        draw = true;
+                        won = true;
+                    }
+                 //   if (emptyAmount != 0 && (xAmount >= 3 && oAmount >= 4 || xAmount >= 4 && oAmount >= 3)){
+                  ///      notFinished = true;
+                  //      won=true;
+                   // }
+                    if (!xWon && !oWon &&emptyAmount !=0){
+                        notFinished=true;
                         won=true;
                     }
                     //}
@@ -86,6 +131,7 @@ public class Main {
 
                 } else {
                     System.out.println("Coordinates should be from 1 to 3!");
+                    loop=true;
                     continue;
                 }
                 if (isChanged) {
@@ -93,31 +139,24 @@ public class Main {
                 } //else {
                 //  System.out.println("This cell is occupied! Choose another one!");
                 // }
-                sc.close();
+
 
             } catch (InputMismatchException e) {
                 System.out.println("You should enter numbers!");
+                loop=true;
                 //break;
             }
-
-        } while (!won);
+        } while (!won || loop);
 
 
         if (xWon) {
             System.out.println("X wins");
         } else if (oWon) {
             System.out.println("O wins");
+        } else if (notFinished) {
+            System.out.println("Game not finished");
         } else if (draw){
             System.out.println("Draw");
-
-      /*  if (!xWon && !oWon && !empty){
-            System.out.println("Draw");
-        }
-        else if (xWon) {
-            System.out.println("X won");
-        } else if (oWon) {
-            System.out.println("O won");
-      */
         }
 
     }
